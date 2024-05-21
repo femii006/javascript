b55d49a681d60473729f8bd7a8821bd7d3d99afa
femii006 <150226846+femii006@users.noreply.github.com>
2024-05-21 14:45:07 +0530

    Add files via upload

---
+++ femina (javas)/function.js
@@ -0,0 +1,10 @@
+// function add(a,b){
+//     let c=a+b;
+//     console.log(c)
+// }
+// add(1,2);
+
+function fullname(fname,lname,){
+    console.log(`${fname} ${lname}`);
+}
+add(`femina`,`fathima`)
---
+++ femina (javas)/script.js
@@ -0,0 +1,88 @@
+// console.log("Hello javascript!!!");
+// let a=1;
+// let b=2;
+// console.log(a+b);
+// let a=10;
+// let b=20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a%b);
+
+// postfix increment
+// let a=1;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+
+// prefix increment
+// let f=1;
+// let k=++f;
+// console.log(f);
+// console.log(k);
+
+// let car={
+//     model: "swift",
+//     color: "grey" 
+
+// };
+// console.log(car);
+// console.log(car.model);
+
+// let person= {
+//     name:"femina fathima",
+//     address: {
+//         street:"xyz",
+//         place:"pkd",
+//         pin:123
+//     },
+//     age:18
+// }
+// // console.log(person.address.place);
+// console.log(person.age);
+
+// var toothbrush=new Object();
+// toothbrush.name="sensodine";
+// toothbrush.color="green";
+// console.log(toothbrush);
+
+// concatination
+//  let fname ="femina";
+//  let lname ="fathima";
+//  console.log(`${fname} ${lname}`)
+//  let m=5;
+//  let n=6;
+//  console.log(`the sum is ${5}${6}`)
+//  console.log(`The sum is ${m+n}`)
+
+//array
+// let fruits=["Apple","Mango","Orange",23]
+// console.log(fruits)
+// console.log(fruits[0])
+// console.log(fruits.length);
+// fruits.push(53);
+// console.log(fruits);
+// fruits[2]="pineapple";
+// console.log(fruits);
+// fruits.unshift("pepsi");
+// console.log(fruits);
+// fruits.pop("pepsi");
+// console.log(fruits);
+let BSc=[
+ {
+    name:"femina",
+    age:18,
+ },
+ {
+    name:"kripa",
+    age:18,
+ },   
+ {
+    name:"adhithya",
+    age:19,
+ },
+];
+console.log(BSc[0].age);
+
